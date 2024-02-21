7dbe90c2b08764fb9e6667466cbaa3f541be6a1a
sreesanjeevkg <hbksanjeev@gmail.com>
2024-02-21 00:12:01 +0000

    Raw data ingestion pipeline implementation

--- mage/stumpsndbails/data_exporters/loadrawdatatogcs.py
+++ mage/stumpsndbails/data_exporters/loadrawdatatogcs.py
@@ -5,32 +5,35 @@ from pandas import DataFrame
 from os import path
 import glob
 import pandas as pd
+import csv
+from google.cloud import storage
+import os
 
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
 
 @data_exporter
-def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
-    """
-    Template for exporting data to a Google Cloud Storage bucket.
-    Specify your configuration settings in 'io_config.yaml'.
-
-    Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
-    """
-    config_path = path.join(get_repo_path(), 'io_config.yaml')
-    config_profile = 'default'
+def export_data(data, *args, **kwargs):
     destination = "/home/src/rawData"
-    csv_files = glob.glob(f"{destination}/*[0-9].csv")
+    csv_files = glob.glob(f"{destination}/*.csv")
 
     bucket_name = 'stumpsndbails_storage_bucket'
+    project_id = 'stumpsndbails'
     object_key_prefix = 'raw/'
+    counter = 0
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/secrets/service-account.json"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
 
     for csv_file in csv_files:
-        df = pd.read_csv(csv_file)
+        counter += 1
+        if (counter % 1000 == 0):
+            print(counter, "files uploaded") 
         object_key = f"{object_key_prefix}{csv_file.split('/')[-1]}"
-        GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
-        df,
-        bucket_name,
-        object_key
-    )
+        blob = bucket.blob(object_key)
+        blob.upload_from_filename(csv_file)
+    
+    print("Raw Data Ingested successfully")

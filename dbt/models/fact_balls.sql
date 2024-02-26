b5c26283cab629762e2d5e1fa49c53643468f72a
Sree Sanjeev <sreesanjeevkg@gmail.com>
2024-02-26 19:43:01 +0000

    dimension matches and fact balls

--- dbt/models/fact_balls.sql
+++ dbt/models/fact_balls.sql
@@ -4,24 +4,71 @@
     )
 }}
 
-with deliveries as (select match_id,
+WITH deliveries AS (
+    SELECT 
+        match_id,
+        innings,
+        SPLIT(CAST(ball AS string), '.')[OFFSET(0)] AS overs,
+        SPLIT(CAST(ball AS string), '.')[OFFSET(1)] AS ball,
+        striker,
+        non_striker,
+        bowler,
+        runs_off_bat,
+        extras,
+        wides,
+        noballs,
+        byes,
+        legbyes,
+        penalty,
+        wicket_type,
+        player_dismissed,
+        other_wicket_type,
+        other_player_dismissed
+    FROM 
+        {{ source('core', 'ball_by_ball') }}
+),
+players AS (
+    SELECT 
+        player_name, 
+        player_id
+    FROM 
+        {{ ref("dim_players") }}
+),
+matches AS (
+    SELECT 
+        match_id 
+    FROM 
+        {{ ref("dim_matches") }}
+) 
+SELECT 
+    matches.match_id,
     innings,
-    split(ball,'.')[0]::int + 1 as 'over',
-    split(ball,'.')[1]::int as ball,
-    striker,
-    non_striker,
-    bowler,
+    cast overs as integer,
+    cast ball as integer,
+    b1.player_id AS striker,
+    b2.player_id AS non_striker,
+    b3.player_id AS bowler,
     runs_off_bat,
     extras,
-    wides,
-    noballs,
-    byes,
-    legbyes,
-    penalty,
-    wicket_type,
-    player_dismissed,
-    other_wicket_type,
-    other_player_dismissed
- from {{ source('core','ball_by_ball')}})
- players as 
- select * from deliveries
+    CASE 
+        WHEN wides IS NOT NULL THEN 'WIDE' 
+        WHEN noballs IS NOT NULL THEN 'NOBALL' 
+        WHEN byes IS NOT NULL THEN 'BYE' 
+        WHEN legbyes IS NOT NULL THEN 'LEGBYES' 
+        WHEN penalty IS NOT NULL THEN 'PENALTY' 
+    END AS extras_type,
+    CASE 
+        WHEN wicket_type is null THEN 'N' 
+        ELSE 'Y' 
+    END AS wicket_flg,
+    wicket_type
+FROM 
+    deliveries
+INNER JOIN 
+    matches ON CAST(deliveries.match_id AS string) = CAST(matches.match_id AS string)
+INNER JOIN 
+    players b1 ON deliveries.striker = b1.player_name
+INNER JOIN 
+    players b2 ON deliveries.non_striker = b2.player_name
+INNER JOIN 
+    players b3 ON deliveries.bowler = b3.player_name
